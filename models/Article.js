1fde5b7a4b885fd8c761a76cf80c77eec76c5d9d
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2019-01-11 00:41:11 -0600

    does not scrap

--- models/Article.js
+++ models/Article.js
@@ -9,16 +9,44 @@ var ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: true
+
   },
   // `link` is required and of type String
   link: {
     type: String,
-    required: true
+    required: true,
+    unique: true
+
+  },
+
+  summary: {
+    type: String,
+    required: false,
+    unique: false
+  },
+
+  byline: {
+    type: String,
+    required: false,
+    unique: false
+  },
+
+  isSaved: {
+    type: Boolean,
+    default: false,
+    required: false,
+    unique: false
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
+  // NOTE I SAW THIS WRITTEN DIFFERENTLY SOMEWHERE ELSE
+//   note: {
+//     type: [{ type: Schema.Types.ObjectId, ref: 'Note'}],
+//   }
+// });
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
