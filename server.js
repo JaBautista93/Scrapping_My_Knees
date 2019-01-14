b2c0d8e231e658cb41c0195bef7ae6da8fbfa281
John-Anthony <41270598+JaBautista93@users.noreply.github.com>
2019-01-14 13:17:11 -0600

    merg conflicts

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var request = require('request');
 var cheerio = require('cheerio');
 
-
 // Initialize Express
 var app = express();
 app.use(logger('dev'));
@@ -43,53 +42,11 @@ db.once('open', function() {
 // Routes
 var Comment = require('./models/Comment.js');
 var Article = require('./models/Article.js');
-var router = require('./controller/controller.js');
+var router = require('./controllers/controller.js');
 app.use('/', router);
 
 // Launch App
 var port = process.env.PORT || 8080;
 app.listen(port, function(){
   console.log('Running on port: ' + port);
-});
-
-// Dependencies
-const express = require("express");
-const bodyParser = require("body-parser"); //JSON responses
-const mongoose = require("mongoose"); //Mongo object modelling 
-const request = require("request"); //Makes http calls
-const cheerio = require("cheerio"); //Scraper
-
-// Require all models
-const db = require("./models");
-
-// Port configuration for local/Heroku
-const PORT = process.env.PORT || process.argv[2] || 8080;
-
-// Initialize Express
-const app = express();
-
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Handlebars
-const exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
-// Controllers
-const router = require("./controllers/api.js");
-app.use(router);
-// Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
-
-// Start the server
-app.listen(PORT, function () {
-    console.log(`This application is running on port: ${PORT}`);
 });
